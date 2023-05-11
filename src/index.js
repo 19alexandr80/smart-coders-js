2582fc80f9ce9a39ee17bba6238eb4e649fcb1c4
19alexandr80 <usanka1980@gmail.com>
2023-05-11 16:23:24 +0300

    notifix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './js/axiosApi';
-
 import './js/authmodal';
+import './js/main';
 
 // const throttle = require('lodash.throttle');
+// ====================
+// import Notiflix from 'notiflix';
 
-import './js/main';
+// // one by one
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Block } from 'notiflix/build/notiflix-block-aio';
+// ==========================
