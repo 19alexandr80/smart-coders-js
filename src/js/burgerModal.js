535ab287cfcf7f49f5756e95fd61933d3fe6de95
19alexandr80 <usanka1980@gmail.com>
2023-05-14 21:53:05 +0300

    okk

--- src/js/burgerModal.js
+++ src/js/burgerModal.js
@@ -2,4 +2,5 @@ const burgerEl = document.querySelector('.burger-menu-header');
 const burgerModalEl = document.querySelector('.modal-burger-menu');
 burgerEl.addEventListener('click', () => {
   burgerModalEl.classList.toggle('modal-burger-menu-off');
+  burgerEl.classList.toggle('active');
 });
