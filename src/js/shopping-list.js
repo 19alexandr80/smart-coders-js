8cc7159ead53843d842513af81b718a1a88b2bb3
Oleksii Nikolaiev <114774361+PDigitator@users.noreply.github.com>
2023-05-16 20:23:17 +0300

    Revert "Shopping list card"

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,23 +1,15 @@
 // import { axiosApiBooks } from './axiosApi';
-
-import amazon from '../images/shops/amazon@1x.png';
-import amazon2x from '../images/shops/amazon@2x.png';
-import apple from '../images/shops/book@1x.png';
-import apple2x from '../images/shops/book@2x.png';
-import bookShop from '../images/shops/books@1x.png';
-import bookShop2x from '../images/shops/books@2x.png';
-
-
 import { DataFirebase } from './firebaseInteraction.js';
 const dataFirebase = new DataFirebase();
-
 const refs = {
-  shopListContainer: document.querySelector('.shop-list__container'),
+  shopListContainer: document.querySelector('.container__shopping-list'),
   imgEmptyPage: document.querySelector('.imgEmptyPage'),
   textEmptyPage: document.querySelector('.textEmptyPage'),
   container: document.querySelector('.shop-list__container'),
+  // btnTrash: document.querySelector('[data-name="btn-trash"]'),
   cards: document.querySelector('.shop-list__cards'),
 };
+// const btnTrash = document.querySelector('.btn-trash-box');
 
 // =========================================================================================
 
@@ -33,7 +25,7 @@ if (!dataBookShop) {
     return dataBookShop[kye];
   });
 }
-console.log(dataBookRender);
+
 // =========================================================================================
 
 // при відкритті сторінки, викликається функція
@@ -56,43 +48,23 @@ function onOpenPage() {
   }
 }
 
-console.log(dataBookRender);
 function makeMarkupBook(dataBookRender) {
-  refs.cards.classList.remove('is-hidden');
-
-  const markup = dataBookRender.map(
-    ({ id, bookImg, author, title, description, listName, buyLinks }) => {
-      let amazonLink = '';
-      let appleBookLink = '';
-      let bookShopLink = '';
-      // David Burroughs was once a devoted father to his three-year-old son Matthew, living a dream life just a short drive away from the working-class suburb where he and his wife, Cheryl, first fell in love--until one fateful night when David woke suddenly to discover Matthew had been murdered while David was asleep just down the hall
-      buyLinks.forEach(link => {
-        if (link.name === 'Amazon') {
-          amazonLink = link.url;
-        }
-        if (link.name === 'Apple Books') {
-          appleBookLink = link.url;
-        }
-        if (link.name === 'Bookshop') {
-          bookShopLink = link.url;
-        }
-      });
+  const markup = dataBookRender
+    .map(({ id, bookImg, author, title, description, listName }) => {
       return ` 
       <li class="shop-list__one-card" data-id=${id}>
         <img class="shop-list__img" src=${bookImg} alt="${title}" />
         <div class="shop-list__text-container">
         <h2 class="shop-list__title-book">${title}</h2>
         <h3 class="shop-list__category-name">${listName}</h3>
-        <p class="shop-list__descr">${description}</p>
+        <p class="shop-list__descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum blanditiis quo similique placeat aut a.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum blanditiis quo similique placeat aut a.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum blanditiis quo similique placeat aut a.</p>
         <h3 class="shop-list__author">${author}</h3>
         </div>
-        
-        <ul class='shop-list__links-group'>
-        <li class='shop-list__links'><a href=${amazonLink} target="_blank"> <img  src='${amazon}' srcset="${amazon} 1x, ${amazon2x} 2x" rel="noopener noreferrer nofollow" aria-label="Amazon link"></a></li>
-        <li class='shop-list__links'><a href=${appleBookLink} target="_blank"> <img  src='${apple}' srcset="${apple} 1x, ${apple2x} 2x" rel="noopener noreferrer nofollow" aria-label="Apple Books link"'></a></li>
-        <li class='shop-list__links'><a href=${bookShopLink} target="_blank"> <img  src='${bookShop}' srcset="${bookShop} 1x, ${bookShop2x} 2x" rel="noopener noreferrer nofollow" aria-label="Amazon link"></a></li>
+        <ul>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
         </ul>
-        
        
       <div>
     <button class="btn-trash-box" type="button" data-name="btn-trash" data-id=${id}>
@@ -102,57 +74,10 @@ function makeMarkupBook(dataBookRender) {
         </svg>
       </span>
     </button>
+   
   </div>
   </li>
     `;
-
-    }
-  );
-  //     .join('');
-
-  //   refs.cards.insertAdjacentHTML('beforeend', markup);
-  // }
-  // вставляємо отриману розмітку до контейнера
-  refs.cards.innerHTML = markup.join('');
-}
-
-// видаляємо книгу з localStorage та з розмітки сторінки
-refs.shopListContainer.addEventListener('click', onRemoveCard);
-
-function onRemoveCard(evt) {
-  console.log(evt.target);
-  if (evt.target.nodeName !== 'use') {
-    return;
-  }
-
-  const card = evt.target.closest('.shop-list__one-card');
-  const id = card.dataset.id;
-
-  // видаляємо книгу з localStorage
-  const bookShopKeys = Object.keys(dataBookShop);
-
-  bookShopKeys.forEach(key => {
-    if (dataBookShop[key].id === id) {
-      delete dataBookShop[key];
-    }
-  });
-
-  localStorage.setItem('shopingList', JSON.stringify(dataBookShop));
-
-  // видаляємо книгу з розмітки сторінки
-  event.target.closest('.shop-list__one-card').remove();
-
-  // якщо всі книги видалено, генерується розмітка пустої сторінки
-  if (!refs.cards.children.length) {
-    refs.cards.classList.add('is-hidden');
-    refs.container.innerHTML = `
-      <p class="textEmptyPage">
-        This page is empty, add some books and proceed to order.
-      </p>
-      <img class="imgEmptyPage" src="./src/images/is-empty@1x.png" alt="" />
-    `;
-  }
-=======
     })
     .join('');
 
@@ -170,28 +95,70 @@ async function onBtnTrash(e) {
   const id = e.target.closest('li').dataset.id;
   await dataFirebase.deleteBook(id);
   window.location.reload();
-
 }
 
-// const card = document.querySelector('.shop-list__one-card');
-// const btnTrash = document.querySelector('[data-name="btn-trash"]');
+// const books = {
+//   _id: 1,
+//   book_image: './src/images/stopper116@1x.png',
+//   author: 'bla Bla',
+//   title: 'I will find you',
+//   category: 'Hardcover fiction',
+//   description:
+//     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.',
+// };
 
-// refs.cards.addEventListener('click', onBtnTrash);
+// function makeMarkupBook({
+//   _id,
+//   title,
+//   author,
+//   book_image,
+//   // description,
+//   list_name,
+//   //   buy_links,
+// }) {
+//   // let amazonLink = '';
+//   // let appleBookLink = '';
+//   // let bookShopLink = '';
 
-// function onBtnTrash(e) {
-//   if (e.target.classList.contains('btn-trash-box')) {
-//     console.log(e.target);
-//     card.remove();
-//   }
+//   // buy_links.forEach(link => {
+//   //   if (link.name === 'Amazon') {
+//   //     amazonLink = link.url;
+//   //   }
+//   //   if (link.name === 'Apple Books') {
+//   //     appleBookLink = link.url;
+//   //   }
+//   //   if (link.name === 'Bookshop') {
+//   //     bookShopLink = link.url;
+//   //   }
+//   // });
+
+//   const markup = `<ul class="shop-list__cards">
+//             <li class="shop-list__one-card" data-id=${_id}>
+//               <img class="shop-list__img" src="${book_image}" alt="${title}" />
+//               <div class="shop-list__text-container">
+//               <h2 class="shop-list__title-book">${title}</h2>
+//               <h3 class="shop-list__category-name">${list_name}</h3>
+//               <p class="shop-list__descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, soluta.</p>
+//               <h3 class="shop-list__author">${author}</h3>
+//               </div>
+//               <ul>
+//                 <li><a href=""></a></li>
+//                 <li><a href=""></a></li>
+//                 <li><a href=""></a></li>
+//               </ul>
+//               <button></button>
+
+//             <div style="padding: 20px; background-color: #f6f6f6">
+//           <button class="btn-trash-box" type="button">
+//             <span class="btn-icn-wrap">
+//               <svg width="18" height="18">
+//                 <use href="./images/icons.svg#trash"></use>
+//               </svg>
+//             </span>
+//           </button>
+//           </li>
+//         </div>
+//           </ul>`;
+//   console.log(markup);
+//   refs.container.insertAdjacentHTML('beforeend', markup);
 // }
-
-// modal.addEventListener('click', event => {
-//   event.stopPropagation();
-// });
-
-// console.log(oneCard);
-// refs.container.innerHTML = `
-// // <p class="textEmptyPage">
-// //   This page is empty, add some books and proceed to order.
-// // </p>
-// // <img class="imgEmptyPage" src="./images/is-empty@1x.png" alt="" />`;
