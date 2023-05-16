4b972d24228cd171028647a8197b8e5689ccc134
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-05-16 05:08:36 +0300

    Upgrade import

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -17,8 +17,9 @@ if (window.screen.width < 768) {
 
 const gallery = document.querySelector('.gallery');
 
-const STORAGE_KEY = 'test'; //!!!!
-localStorage.setItem(STORAGE_KEY, JSON.stringify(galleryItems)); //!!!!
+// const STORAGE_KEY = 'test'; //!!!!
+const STORAGE_KEY = 'shopingList';
+// localStorage.setItem(STORAGE_KEY, JSON.stringify(galleryItems)); //!!!!
 
 getData();
 
@@ -53,18 +54,41 @@ export function getNewDataBatch(currentPage) {
 
 export function makeImageGallery(galleryItems) {
   return galleryItems
-    .map(({ preview, original, description }) => {
+    .map(({ bookImg, author, listName, title }) => {
       return `
       <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
+        <a class="gallery__link" href="#">
             <img
                 class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}" width=800px height=800px
+                src="${bookImg}"
+                data-source="${listName}"
+                alt="${title}" width=800px height=800px
             />
         </a>
+        <p>${author}</p>
+        <p>${title}</p>
+        <p>${listName}</p>
       </li>`;
     })
     .join('');
 }
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// author
+// :
+// "Jeannette Walls"
+// bookImg
+// :
+// "https://storage.googleapis.com/du-prd/books/images/9781501117299.jpg"
+// buyLinks
+// :
+// [{name: "Amazon", url: "https://www.amazon.com/dp/1501117297?tag=NYTBSREV-20"},…]
+// id
+// :
+// "643282b1e85766588626a0a0"
+// listName
+// :
+// "Hardcover Fiction"
+// title
+// :
+// "HANG THE MOON"
