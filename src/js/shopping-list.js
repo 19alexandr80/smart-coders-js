f4181fd5c6e5744db8876fb6a1b35e0f7bd9e025
anasteiisha <anastasiyafrantseva5@gmail.com>
2023-05-16 11:48:11 +0300

    update

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -5,84 +5,92 @@ const refs = {
   imgEmptyPage: document.querySelector('.imgEmptyPage'),
   textEmptyPage: document.querySelector('.textEmptyPage'),
   container: document.querySelector('.shop-list__container'),
-  containerEmptyPage: document.querySelector('.shop-list__empty-page'),
-  btnTrash: document.querySelector('.btn-trash-box'),
+  // btnTrash: document.querySelector('[data-name="btn-trash"]'),
+  cards: document.querySelector('.shop-list__cards'),
 };
+// const btnTrash = document.querySelector('.btn-trash-box');
 
 // =========================================================================================
 
 const dataBookShop = JSON.parse(localStorage.getItem('shopingList'));
+console.log(dataBookShop);
 const bookShopKyes = Object.keys(dataBookShop);
+
 const dataBookRender = bookShopKyes.map(kye => {
   return dataBookShop[kye];
 });
 console.log(dataBookRender);
 // =========================================================================================
 
-// refs.shopListBtn.addEventListener('click', onBtnClick);
-// refs.btnTrash.addEventListener('click', onBtnTrash);
-console.log(refs.btnTrash);
-onBtnClick();
-function onBtnClick() {
+// при відкритті сторінки, викликається функція
+onOpenPage();
+function onOpenPage() {
   console.log('gooood');
+  // якщо в local storage є хоча б одна книга, стираєтьсч розмітка пустої сторінки і малюється розмітка книги
   if (dataBookRender.length >= 1) {
     refs.imgEmptyPage.remove('.imgEmptyPage');
     refs.textEmptyPage.remove('.textEmptyPage');
     makeMarkupBook(dataBookRender);
   }
-  // if ((dataBookRender.length = 0)) {
-  //   // refs.containerEmptyPage.innerHTML = `
-  //   // <p class="textEmptyPage">
-  //   //   This page is empty, add some books and proceed to order.
-  //   // </p>
-  //   // <img class="imgEmptyPage" src="./images/is-empty@1x.png" alt="" />`;
-  // }
+  // якщо в local storage немає жодної книги, малюється розмітка пустої сторінки
+  if (!dataBookRender) {
+    refs.containerEmptyPage.innerHTML = `
+    <p class="textEmptyPage">
+      This page is empty, add some books and proceed to order.
+    </p>
+    <img class="imgEmptyPage" src="./images/is-empty@1x.png" alt="" />`;
+  }
 }
 
-// function onBtnTrash() {
-//   refs.container.innerHTML = `
-//   <p class="textEmptyPage">
-//     This page is empty, add some books and proceed to order.
-//   </p>
-//   <img class="imgEmptyPage" src="./images/is-empty@1x.png" alt="" />`;
-// }
-
-function makeMarkupBook(dataBookRender) {
+async function makeMarkupBook(dataBookRender) {
   console.log('bla bla');
   const markup = dataBookRender
     .map(({ id, bookImg, author, title, description, listName }) => {
-      return ` <ul class="shop-list__cards">
+      return ` 
       <li class="shop-list__one-card" data-id=${id}>
         <img class="shop-list__img" src=${bookImg} alt="${title}" />
         <div class="shop-list__text-container">
-        <h2 class="shop-list__title">${title}</h2>
+        <h2 class="shop-list__title-book">${title}</h2>
         <h3 class="shop-list__category-name">${listName}</h3>
-        <p class="shop-list__descr">${description}</p>
-        <h3 class="author">${author}</h3>
+        <p class="shop-list__descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum blanditiis quo similique placeat aut a.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum blanditiis quo similique placeat aut a.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum blanditiis quo similique placeat aut a.</p>
+        <h3 class="shop-list__author">${author}</h3>
         </div>
         <ul>
           <li><a href=""></a></li>
           <li><a href=""></a></li>
           <li><a href=""></a></li>
         </ul>
-        <button></button>
-
-      <div style="padding: 20px; background-color: #f6f6f6">
-    <button class="btn-trash-box" type="button">
+       
+      <div>
+    <button class="btn-trash-box" type="button" data-name="btn-trash">
       <span class="btn-icn-wrap">
         <svg width="18" height="18">
-          <use href="./images/icons.svg#trash"></use>
+          <use href="/icons.adfc4680.svg#trash"></use>
         </svg>
       </span>
     </button>
-    </li>
+   
   </div>
-    </ul>`;
+  </li>
+    `;
     })
     .join('');
-  console.log(markup);
-  console.log(refs.container);
-  refs.container.innerHTML = markup;
+
+  refs.cards.insertAdjacentHTML('beforeend', markup);
+  const btnTrash = document.querySelector('[data-name="btn-trash"]');
+
+  btnTrash.addEventListener('click', onBtnTrash);
+}
+const itemShopList = document.querySelector('.shop-list__one-card');
+
+function onBtnTrash(e) {
+  itemShopList.classList.add('is-hidden');
+  console.log('hbkhbj');
+  // refs.container.innerHTML = `
+  // // <p class="textEmptyPage">
+  // //   This page is empty, add some books and proceed to order.
+  // // </p>
+  // // <img class="imgEmptyPage" src="./images/is-empty@1x.png" alt="" />`;
 }
 
 // const books = {
